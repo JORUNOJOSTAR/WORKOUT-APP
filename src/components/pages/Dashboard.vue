82b84c7c481858c3b064f065a0e86db7abde1f02
JOJO <nwin919191@gmail.com>
2025-07-09 21:00:47 +0900

    complete implement basic design of workout grid and dashboard page

--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -1,8 +1,49 @@
 <script setup>
+    import Grid from '../Grid.vue'
+    import { gymHealthFacts } from '../../utils';
+    
+    const randomNumber =  Math.floor(Math.random()*gymHealthFacts.length);
+    const todayFact = gymHealthFacts[randomNumber];
 </script>
 
 <template>
+    <section idd="dashboard">
+        <div class="card tip-container">
+            <h2>Welcome JOE</h2>
+            <div>
+                <p class="tip">
+                    <strong>Daily Tip</strong><br/>{{ todayFact }}
+                </p>
+            </div>
+            <button>Start Workout &rarr;</button>
+        </div>
+        <Grid/>
+    </section>
 </template>
 
 <style scoped>
+    .tip-container,
+    .tip-container div,
+    #dashboard{
+        display: flex;
+    }
+
+    .tip-container,
+    #dashboard{
+        flex-direction: column;
+    }
+
+    #dashboard{
+        gap:2rem;
+    }
+
+    .tip-container{
+        gap: 0.5rem;
+    }
+
+    @media(min-width: 640px){
+        .tip-container{
+            gap:1rem;
+        }
+    }
 </style>
